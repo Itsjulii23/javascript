dd882975932ee4a79aabb377892aea689e5c488b
Julian <j.campillosmartinez@gmaik.com>
2023-09-25 09:46:11 +0200

    h

---
+++ 01-basics/ut-2/exercise-2.js
@@ -0,0 +1,9 @@
+alert("This is an alert MessageChannel, click me to remove on!!!");
+
+conf1=confirm ("Show me in an alert if you clicked on ok or false");
+
+confirm("You clicked on "+conf1+" click me to move on!!!");
+
+prompt1=prompt("Show me in an alert the message typed");
+
+alert("You typed "+prompt1+" click me to move on");
